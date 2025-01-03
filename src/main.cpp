7b8ba71540f84d9a6fd9c45644862ee3a852c5a2
Pavel Tuktarov <tuktarov.p@yandex.ru>
2025-01-03 16:53:35 +0800

    Fix 'SDL_MAIN_HANDLED'

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <bitset>
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_log.h>
 
@@ -6,28 +7,31 @@
 
 int main(int argc, char* argv[])
 {
-    SDL_log
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
-        SDL_Log("SDL_Init Error: %s\n", SDL_GetError())
+        SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
-    SDL_Window *window = SDL_CreateWindow("CHIP 8", 100, 100, 64, 32, SDL_WINDOW_SHOWN)
+    SDL_Window *window = SDL_CreateWindow("CHIP 8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 320, SDL_WINDOW_SHOWN);
     if (window == nullptr)
     {
-        SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError())
-        return 1;
+        SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return 2;
     }
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
     
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
     if (renderer == nullptr)
     {
         SDL_Log("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-        return 1;
+        return 3;
     }
+
+    SDL_Delay(2000);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
