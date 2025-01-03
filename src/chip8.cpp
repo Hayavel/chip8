010150e97d4b2ca4594dd026756b4bd85a26dfd4
Pavel Tuktarov <tuktarov.p@yandex.ru>
2025-01-04 01:37:38 +0800

    Write logic for 29 chip8 opcode; change 'loadProgram'

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -7,14 +7,23 @@ void Chip8::loadFontSet()
     for (size_t i=0; i < 80; ++i)
         ram.memory[i] = chip8_fontset[i];
 }
-void Chip8::loadProgram(const char* program)
+bool Chip8::loadProgram(const char* program)
 {
-    std::ifstream file(program, std::ios::binary);
-    char* buffer;
-    size_t bufferSize;
-    file.read(buffer, bufferSize);
-    for (size_t i=0; i < bufferSize; ++i)
-        ram.memory[512 + i] = buffer[i];
+    try
+    {
+        std::ifstream file(program, std::ios::binary);
+        char* buffer;
+        size_t bufferSize;
+        file.read(buffer, bufferSize);
+        for (size_t i=0; i < bufferSize; ++i)
+            ram.memory[512 + i] = buffer[i];
+        return 1;
+    }
+    catch (...)
+    {
+        return 0;
+    }
+
 }
 char* Chip8::emulateCycle()
 {
@@ -48,7 +57,9 @@ char* Chip8::emulateCycle()
         // 0NNN: Calls machine code routine at address NNN. Not necessary for most ROMs
         case 0x1000: // 1NNN: Jumps to address NNN
         {
-
+            ram.stack[ram.sp] = cpu.pc;
+            ++ram.sp;
+            cpu.pc = ram.opcode & 0x0FFF;
             break;
         }
         case 0x2000: // 2NNN: Calls subroutine at NNN
@@ -60,22 +71,35 @@ char* Chip8::emulateCycle()
         }
         case 0x3000: // 3Xkk: Skips the next instruction if VX equals kk (usually the next instruction is a jump to skip a code block)
         {
+            if (cpu.V[(ram.opcode & 0x0F00) >> 8] == (ram.opcode & 0x00FF))
+                cpu.pc += 2;
+            cpu.pc += 2;
             break;
         }
         case 0x4000: // 4Xkk: Skips the next instruction if VX does not equal kk (usually the next instruction is a jump to skip a code block)
         {
+            if (cpu.V[(ram.opcode & 0x0F00) >> 8] != (ram.opcode & 0x00FF))
+                cpu.pc += 2;
+            cpu.pc += 2;
             break;
         }
         case 0x5000: // 5XY0: Skips the next instruction if VX equals VY (usually the next instruction is a jump to skip a code block)
         {
+            if (cpu.V[(ram.opcode & 0x0F00) >> 8] = cpu.V[(ram.opcode & 0x00F0) >> 4])
+                cpu.pc += 2;
+            cpu.pc += 2;
             break;
         }
         case 0x6000: // 6Xkk: Sets VX to kk
         {
+            cpu.V[(ram.opcode & 0x0F00) >> 8] = ram.opcode & 0x00FF;
+            cpu.pc += 2;
             break;
         }
         case 0x7000: // 7Xkk: Adds kk to VX (carry flag is not changed)
         {
+            cpu.V[(ram.opcode & 0x0F00) >> 8] += ram.opcode & 0x00FF;
+            cpu.pc += 2;
             break;
         }
         case 0x8000:
@@ -84,18 +108,26 @@ char* Chip8::emulateCycle()
             {
                 case 0x0000: // 8XY0: Set VX to the value of VY
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] = cpu.V[(ram.opcode & 0x00F0) >> 4];
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x0001: // 8XY1: Sets VX to VX or VY
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] = cpu.V[(ram.opcode & 0x0F00) >> 8] | cpu.V[(ram.opcode & 0x00F0) >> 4];
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x0002: // 8XY2: Sets VX to VX and VY
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] = cpu.V[(ram.opcode & 0x0F00) >> 8] & cpu.V[(ram.opcode & 0x00F0) >> 4];
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x0003: // 8XY3: Sets VX to VX xor VY
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] = cpu.V[(ram.opcode & 0x0F00) >> 8] ^ cpu.V[(ram.opcode & 0x00F0) >> 4];
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x0004: // 8XY4: Adds VY to VX. VF is set to 1 when there's an overflow, and to 0 when there is not
@@ -110,18 +142,44 @@ char* Chip8::emulateCycle()
                 }
                 case 0x0005: // 8XY5: VY is subtracted from VX. VF is set to 0 when there's an underflow, and 1 when there is not (i.e. VF set to 1 if VX >= VY and 0 if not)
                 {
+                    if (cpu.V[(ram.opcode & 0x0F00) >> 8] > cpu.V[(ram.opcode & 0x00F0) >> 4])
+                        cpu.V[0xF] = 1;
+                    else
+                        cpu.V[0xF] = 0;
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] -= cpu.V[(ram.opcode & 0x00F0) >> 4];
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x0006: // 8XY6: Shifts VX to the right by 1, then stores the least significant bit of VX prior to the shift into VF
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] >>= 1;
+                    if (((cpu.V[(ram.opcode & 0x0F00) >> 8]  % 100) % 10) == 1)
+                        cpu.V[0xF] = 1;
+                    else
+                        cpu.V[0xF] = 0;
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] /= 2;
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x0007: // 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's an underflow, and 1 when there is not (i.e. VF set to 1 if VY >= VX)
                 {
+                    if (cpu.V[(ram.opcode & 0x00F0) >> 4] > cpu.V[(ram.opcode & 0x0F00) >> 8])
+                        cpu.V[0xF] = 1;
+                    else
+                        cpu.V[0xF] = 0;
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] = cpu.V[(ram.opcode & 0x00F0) >> 4] - cpu.V[(ram.opcode & 0x0F00) >> 8];
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x000E: // 8XYE: Shifts VX to the left by 1, then sets VF to 1 if the most significant bit of VX prior to that shift was set, or to 0 if it was unset
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] <<= 1;
+                    if ((cpu.V[(ram.opcode & 0x0F00) >> 8] / 100) == 1)
+                        cpu.V[0xF] = 1;
+                    else
+                        cpu.V[0xF] = 0;
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] *= 2;
+                    cpu.pc += 2;
                     break;
                 }
                 default:
@@ -132,6 +190,9 @@ char* Chip8::emulateCycle()
         }
         case 0x9000: // 9XY0: Skips the next instruction if VX does not equal VY (Usually the next instruction is a jump to skip a code block)
         {
+            if (cpu.V[(ram.opcode & 0x0F00) >> 8] != cpu.V[(ram.opcode & 0x00F0) >> 4])
+                cpu.pc += 2;
+            cpu.pc += 2;
             break;
         }
 
@@ -144,14 +205,40 @@ char* Chip8::emulateCycle()
 
         case 0xB000: // BNNN: Jumps to the address NNN plus V0
         {
+            ram.stack[ram.sp] = cpu.pc;
+            ++ram.sp;
+            cpu.pc = (ram.opcode & 0x0FFF) + cpu.V[0];
             break;
         }
         case 0xC000: // CXkk: Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and kk
         {
+            cpu.V[(ram.opcode & 0x0F00) >> 8] = (std::rand()%255) & (ram.opcode & 0x00FF);
+            cpu.pc += 2;
             break;
         }
         case 0xD000: // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
         {
+            unsigned short x = cpu.V[(ram.opcode & 0x0F00) >> 8];
+            unsigned short x = cpu.V[(ram.opcode & 0x00F0) >> 4];
+            unsigned short height = ram.opcode & 0x000F;
+            unsigned short pixel;
+
+            cpu.V[0xF] = 0;
+            for (size_t yline = 0; yline < height; yline++)
+            {
+                pixel = ram.memory[cpu.I + yline];
+                for (size_t xline = 0; xline < 8; xline++)
+                {
+                    if ((pixel & (0x80 >> xline)) != 0)
+                    {
+                        if (display.videoarray[(x + xline + ((y + yline) * 64))] == 1)
+                            cpu.V[0xF] = 1;
+                        display.videoarray[(x + xline + ((y + yline) * 64))] ^= 1;
+                    }
+                }
+            }
+            drawFlag = true;
+            pc += 2;
             break;
         }
         case 0xE000:
@@ -160,10 +247,16 @@ char* Chip8::emulateCycle()
             {
                 case 0x009E: // EX9E: Skips the next instruction if the key stored in VX(only consider the lowest nibble) is pressed (usually the next instruction is a jump to skip a code block)
                 {
+                    if (keyboard.key[cpu.V[(ram.opcode & 0x0F00) >> 8]] != 0)
+                        pc += 2;
+                    pc += 2;
                     break;
                 }
                 case 0x00A1: // EXA1: Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
                 {
+                    if (keyboard.key[cpu.V[(ram.opcode & 0x0F00) >> 8]] == 0)
+                        pc += 2;
+                    pc += 2;
                     break;
                 }
                 default:
@@ -178,10 +271,14 @@ char* Chip8::emulateCycle()
             {
                 case 0x0007: // FX07: Sets VX to the value of the delay timer
                 {
+                    cpu.V[(ram.opcode & 0x0F00) >> 8] = timer.delay_timer;
+                    cpu.pc += 2;
                     break;
                 }
                 case 0x000A: // FX0A: A key press is awaited, and then stored in VX (blocking operation, all instruction halted until next key event, delay and sound timers should continue processing)
                 {
+                    // unsigned char = 
+                    // cpu.V[(ram.opcode & 0x0F00) >> 8] =
                     break;
                 }
                 case 0x0015: // FX15: Sets the delay timer to VX
