62d37f0ceff6632b4f3a7eda592282dc20814518
Pavel Tuktarov <tuktarov.p@yandex.ru>
2025-01-03 18:26:25 +0800

    Chip8 Initialize: clear memory and register reset

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -3,13 +3,31 @@
 
 void Chip8::initialize()
 {
+    cpu.pc = 0x200; // Program counter starts at 0x200
+    ram.opcode = 0; // Reset current opcode
+    cpu.I = 0;      // Reset index register 
+    ram.sp = 0;     // Reset stack pointer
+
+    // Clear display
+    // Clear stack
+    // Clear register V0-VF
+    // Clear memory
+
+    // Load fontset
+    for (size_t i=0; i < 80; ++i)
+        ram.memory[i] = chip8_fontset[i];
     
+    // Reset timer
 }
 void Chip8::loadGame(const char* game)
 {
 
 }
-void Chip8::emulateCycle()
+char* Chip8::emulateCycle()
+{
+
+}
+bool Chip8::updateTimers()
 {
 
 }
